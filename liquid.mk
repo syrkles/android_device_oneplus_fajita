66e94f9203fb82d560c23d3addaa8b8a173570b2
syrkles <syrkles@gmail.com>
2018-11-22 14:36:04 -0500

    Update and rename carbon_fajita.mk to liquid.mk

--- liquid.mk
+++ liquid.mk
@@ -22,13 +22,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fajita device
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
-# Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+
 
 # Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
-PRODUCT_NAME := carbon_fajita
+PRODUCT_NAME := liquid_fajita
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
